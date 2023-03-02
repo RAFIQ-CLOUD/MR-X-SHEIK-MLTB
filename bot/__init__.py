5af2711dc79b0ce9d5f940863bab47ce3639851f
RAFIQ-CLOUD <124174722+RAFIQ-CLOUD@users.noreply.github.com>
2023-03-03 03:01:10 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -674,7 +674,7 @@ if len(BASE_URL) == 0:
 
 UPSTREAM_REPO = environ.get('UPSTREAM_REPO', '')
 if len(UPSTREAM_REPO) == 0:
-   UPSTREAM_REPO = 'https://github.com/RAFIQ-CLOUD/MR-SN-MLTB-TEST'
+   UPSTREAM_REPO = 'https://github.com/RAFIQ-CLOUD/MR-X-SHEIK-MLTB'
 
 UPSTREAM_BRANCH = environ.get('UPSTREAM_BRANCH', '')
 if len(UPSTREAM_BRANCH) == 0:
